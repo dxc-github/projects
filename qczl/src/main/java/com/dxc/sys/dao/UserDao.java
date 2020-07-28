5cc431ae1a88e647035a7ef5395c182f4fc88bdb
dxc <dxc-github>
2020-07-28 20:00:30 +0800

    1111111

---
+++ qczl/src/main/java/com/dxc/sys/dao/UserDao.java
@@ -0,0 +1,22 @@
+package com.dxc.sys.dao;
+
+import com.dxc.sys.entity.User;
+
+public interface UserDao {
+    int deleteByPrimaryKey(Integer userid);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userid);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+    /**
+     * 登陆
+     */
+    User login(User user);
+}
--- qczl/src/main/webapp/index.jsp
+++ qczl/src/main/webapp/index.jsp
@@ -1,5 +1,7 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-<body>
-<h2>Hello World!</h2>
-</body>
+    <head>登录页面</head>
+    <body>
+        <jsp:forward page="login/toLogin.action"></jsp:forward>
+    </body>
 </html>
